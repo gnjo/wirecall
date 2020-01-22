6a2fabcd76f34238e9a36bb33907f6dc30eaca51
gunjyo <35440828+gnjo@users.noreply.github.com>
2020-01-22 19:45:13 +0900

    Update echos.js

--- echos.js
+++ echos.js
@@ -1,4 +1,32 @@
 /////////////////////////////////////////
+//echos={} //core
+//fn={} //util functions
+$$$=void 0 //return //$
+$r0=void 0 //same $$$
+$r1=void 0 //other return
+$r2=void 0 //
+$r3=void 0 // 
+$r4=void 0 // 
+$r5=void 0 // 
+$r6=void 0 // 
+$r7=void 0 // 
+$r8=void 0 //
+$r9=void 0 // 
+$$0="" //message layer 0 
+$$1="" //
+$$2="" //select layer
+$$3="" //
+$$4="" //
+$$5="" //
+$$6="" //
+$$7="" //
+$$8="" //
+$$9="" // 
+$$k=void 0 //latest key
+$$n=void 0 //latest select number
+$$m='free' //now mode, sel|mes|free
+
+/////////////////////////////////////////
 ;(function(root){
  let fn={}
  fn.q=(s,doc=document)=>{return doc.querySelector(s)}
